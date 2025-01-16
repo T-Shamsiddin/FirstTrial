e885edbc56da011ccd13a8486bb62898a2b32612
sh.tohirov <sh.tohirov@newuu.uz>
2025-01-16 10:08:47 +0500

    cout and cin

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    auto lang = "C++";
+    string name;
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    cout << "Enter your name: " << endl;
+    cin>>name;
+
+    // for (int i = 1; i <= 5; i++) {
+    //     std::cout << "i = " << i << std::endl;
+    // }
+
+    return 0;
+}
