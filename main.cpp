09e8795000cff0b00b27a9a37b39696e633b5a1a
sh.tohirov <sh.tohirov@newuu.uz>
2025-01-23 09:52:20 +0500

    lab 1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,61 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    auto lang = "C++";
-    string name;
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    // auto lang = "C++";
+    // std::cout << "Hello and welcome to " << lang << "!\n";
 
+    // Task 1
+    string name;
+    int age;
     cout << "Enter your name: " << endl;
     cin>>name;
+    cout << "Enter your age: ";
+    cin >> age;
+    std::cout << "Your name:" << name << " Your age: " << age << std::endl;
 
-    // for (int i = 1; i <= 5; i++) {
-    //     std::cout << "i = " << i << std::endl;
+    // Task 2
+    // int temp = 0;
+    // while (temp<5) {
+    //     cout << "Number: " << std::setw(5) << temp << endl;
+    //     temp++;
     // }
+
+    // Task 3
+    // int a, b;
+    // cout << "Enter a: ";
+    // cin >> a;
+    // cout << "Enter b: ";
+    // cin >> b;
+    //
+    // const int temp = a;
+    // a = b;
+    // b = temp;
+    // cout << "a: " << a << " b: " << b << endl;
+
+    // Task 4
+    // int n;
+    // cout << "N: ";
+    // cin >> n;
+    //
+    // for (int i = 1; i <= n; i++ ) {
+    //     int a = pow(i, 2);
+    //     cout << "number: " << i << " Square: " << a << endl;
+    // }
+
+    // Task 5
+    // string str;
+    // cout << "Enter a string: " << endl;
+    // cin >> str;
+    //
+    // int len = (50 - str.length()) / 2;
+    // cout << "-" << setw(len) << str << setw(len) << "-";
+
+
+
+
 
     return 0;
 }
