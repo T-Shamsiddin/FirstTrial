f715bbe0ea4bddc85e4658cc204a913ded7d938c
sh.tohirov <sh.tohirov@newuu.uz>
2025-04-10 09:56:33 +0500

    lab 12 part 2

--- lab12.cpp
+++ lab12.cpp
@@ -4,6 +4,44 @@
 #include <iostream>
 using namespace std;
 
+// #define val 33.3;
+// #define number 50;
+// int a;
+// int& fun(){
+//     return a;
+// }
+// int foo(){
+//     return a;
+// }
+
+void sortArray(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i+1; j < size; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int* countDigits(const int& number) {
+    int* c = new int[10]();
+    int n = number;
+    if (n ==0) {
+        c[0]++;
+        return c;
+    }
+
+    while (n>0) {
+        int a = n%10;
+        c[a]++;
+        n/=10;
+    }
+    return c;
+}
+
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -14,7 +52,7 @@ void square(int *a) {
     *a = *a * *a;
 }
 
-// int main() {
+int main() {
 
     // task 1
     // int n;
@@ -102,6 +140,133 @@ void square(int *a) {
     // cout << *pB << endl;
 
     // task 11
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    // }
+    // int *data = a;
+    // for (int i = 0; i < n; i++) {
+    //     if (*data >= a[i]) {
+    //         data = &a[i];
+    //     }
+    // }
+    // cout << *data;
 
-//     return 0;
-// }
+    // task 12
+    // int n1=9,n2=11, *ptr_1= nullptr, *ptr_2= nullptr, k=0;
+    // double t;
+    // ptr_1=&n1;
+    // // ptr_2=&n2;
+    // // *ptr_2=n2;
+    // ptr_2=&n2;
+    // t=val;
+    // k=val;
+    // // foo()=number;
+    // fun()=number;
+    // cout << "n1: " << n1 << " ";
+    // cout << "n2: " << n2 << " ";
+    // cout << "*ptr_1: " << *ptr_1 << " ";
+    // cout << "*ptr_2: " << *ptr_2 << endl;
+    // cout << "t (double from macro val): " << t << endl;
+    // cout << "k (int from macro val): " << k << endl;
+    // cout << "a (after fun()=number): " << a << endl;
+
+    // task 13
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    // }
+    // int *ptr = a;
+    // for (int i = 0; i < n; i++) {
+    //     cout << ptr[i] << " : " << &ptr[i] << endl;
+    // }
+
+    // task 14
+    // int n;
+    // cin >> n;
+    // int *ptr = &n;
+    // cout << *ptr << endl;
+
+    // task 15
+    // int arr[] = {10, 20, 30, 40, 50};
+    // int *ptr = arr;
+    // cout << *ptr << endl;
+    // int i = size(arr);
+    // cout << *(ptr + i-1) << endl;
+
+    // task 16
+    // int n = 75;
+    // int *p = &n;
+    // cout << *p << endl;
+    // *p+=10;
+    // cout << *p << endl;
+    // cout << n << endl;
+
+    // task 17
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    // }
+    // int *start = a;
+    // int size = sizeof(a) / sizeof(a[0]);
+    // int *end = a+size-1;
+    // while (start < end) {
+    //     int temp = *start;
+    //     *start = *end;
+    //     *end = temp;
+    //     start++;
+    //     end--;
+    // }
+    // for (int i = 0; i < size; i++) {
+    //     cout << a[i] << endl;
+    // }
+
+    // task 18
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    // }
+    // int* first = &a[0], *second = nullptr, *third = nullptr;
+    // for (int i = 0; i < n; i++) {
+    //     if (a[i]> *first) {
+    //         third = second;
+    //         second = first;
+    //         first = &a[i];
+    //     }else if ((a[i]>*second || second == nullptr) && &a[i] != first) {
+    //         third = second;
+    //         second = &a[i];
+    //     }else if ((a[i]>*third || third == nullptr) && &a[i] != second && &a[i] != first) {
+    //         third = &a[i];
+    //     }
+    // }
+    // cout << *first << " " << *second << " " << *third << endl;
+
+    // task 19
+    // int n;
+    // cin >> n;
+    // int * result = countDigits(n);
+    // for (int i = 0; i < 10; i++) {
+    //     cout << i << ": " << result[i] << endl;
+    // }
+
+    // task 20
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sortArray(a, n);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
